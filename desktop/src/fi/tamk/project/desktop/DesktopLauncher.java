aeba96c4a86faa493154a16cfde10a932e1e9c7f
pasimakit <pasi.makitalo@tuni.fi>
2019-03-04 16:00:17 +0200

    Initial commit

---
+++ desktop/src/fi/tamk/project/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package fi.tamk.project.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import fi.tamk.project.MainGame;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new MainGame(), config);
+	}
+}
